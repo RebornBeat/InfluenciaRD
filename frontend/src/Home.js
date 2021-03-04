eef0abfb955d3ed1c2697bd964378aa3e9630aef
RebornBeat <christian.lfonts@gmail.com>
2021-03-04 08:30:12 -0400

    End Title Section

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -12,9 +12,12 @@ export default class Home extends Component {
 				<div id="Title_Container">
 					<div id="Title_Wrapper">
 						<span id="Title">Promociona tu negocio en segundos</span>
-						<span>Promociona tu negocio en segundos</span>
+						<span>Pon tu negocio en la vista de mas de 1.6 millones de domincanos</span>
 						<button>Crea Una Cuenta Gratis</button>
 					</div>
+				</div>
+				<div id="marketplace_container">
+				
 				</div>
 			</>
 		);
